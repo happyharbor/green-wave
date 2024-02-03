d6c9b42ada4c06e22599b330700242728f788879
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2024-02-03 15:37:46 +0200

    Move back to Jpg

--- apps/landing-page/src/components/TrustedByBlock/index.tsx
+++ apps/landing-page/src/components/TrustedByBlock/index.tsx
@@ -12,13 +12,13 @@ const TrustedByBlock = ({ t }: TranslationProps) => {
       <Visible height={124}>
         <h3>{t('Trusted by')}</h3>
         <ContentWrapper>
-          <ContentImage image={'colore.webp'} width={6.8675} height={1.63875}></ContentImage>
-          <ContentImage image={'beach_born.webp'} width={6.8675} height={2.74625}></ContentImage>
+          <ContentImage image={'colore.jpg'} width={6.8675} height={1.63875}></ContentImage>
+          <ContentImage image={'beach_born.jpg'} width={6.8675} height={2.74625}></ContentImage>
           <ContentImage image={'plantees.svg'} width={6.86419} height={1.29313}></ContentImage>
-          <ContentImage image={'hasonlul.webp'} width={6.8675} height={1.63875}></ContentImage>
-          <ContentImage image={'colore.webp'} width={6.8675} height={1.63875}></ContentImage>
-          <ContentImage image={'mayas.webp'} width={6.8675} height={3.125}></ContentImage>
-          <ContentImage image={'rekorek.webp'} width={5.49313} height={3.75}></ContentImage>
+          <ContentImage image={'hasonlul.jpg'} width={6.8675} height={1.63875}></ContentImage>
+          <ContentImage image={'colore.jpg'} width={6.8675} height={1.63875}></ContentImage>
+          <ContentImage image={'mayas.jpg'} width={6.8675} height={3.125}></ContentImage>
+          <ContentImage image={'rekorek.jpg'} width={5.49313} height={3.75}></ContentImage>
         </ContentWrapper>
       </Visible>
     </TrustedByBlockSection>
--- apps/landing-page/src/components/WhoAreWeBlock/index.tsx
+++ apps/landing-page/src/components/WhoAreWeBlock/index.tsx
@@ -13,10 +13,10 @@ const WhoAreWeBlock = ({ t, id }: PageBlockProps) => {
           <h2>{t('Who Are We')}</h2>
           <Content>{t('WhoAreWeContent')}</Content>
           <UsSection>
-            <WhoAmIContent image={'iliada.webp'} name={'Iliada'} title={'Marketing Expert'} t={t} />
-            <WhoAmIContent image={'thodoris.webp'} name={'Thodoris'} title={'Marketing Expert'} t={t} />
-            <WhoAmIContent image={'vasia.webp'} name={'Vasia'} title={'UX/UI Designer'} t={t} />
-            <WhoAmIContent image={'giorgos.webp'} name={'Giorgos'} title={'Developer | IT Consultant'} t={t} />
+            <WhoAmIContent image={'iliada.jpg'} name={'Iliada'} title={'Marketing Expert'} t={t} />
+            <WhoAmIContent image={'thodoris.jpg'} name={'Thodoris'} title={'Marketing Expert'} t={t} />
+            <WhoAmIContent image={'vasia.jpg'} name={'Vasia'} title={'UX/UI Designer'} t={t} />
+            <WhoAmIContent image={'giorgos.jpg'} name={'Giorgos'} title={'Developer | IT Consultant'} t={t} />
           </UsSection>
         </ContentWrapper>
       </Visible>
